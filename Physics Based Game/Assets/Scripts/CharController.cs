fbae2d019400c490e483256cb31727cb18298114
emihardej <57240846+emihardej@users.noreply.github.com>
2023-01-05 22:03:41 +0800

    Prototyping Game Mechanics
    Test scene created
    Basic player movement implemented
    Collision detection implemented with test knockable objects

---
+++ Physics Based Game/Assets/Scripts/CharController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    private CharacterController controller;
+
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField] 
+    private float force = 10;
+
+    private bool grounded;
+    private Vector3 jumping;
+    
+    [SerializeField]
+    private float jumpHeight = 1f;
+
+    [SerializeField]
+    private float gravityVal = -9.81f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        grounded = controller.isGrounded;
+        if (grounded)
+        {
+            jumping.y = 0f;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal , 0, vertical).normalized;
+
+        controller.Move(direction * speed * Time.deltaTime);
+
+        // if (direction != Vector3.zero)
+        // {
+        //     controller.Move(direction.normalized * speed * Time.deltaTime);
+        // }
+
+        if (Input.GetButtonDown("Jump") && grounded){
+            jumping.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityVal);
+            grounded = false; 
+            
+        
+        }
+        controller.Move(jumping * Time.deltaTime);
+        jumping.y += gravityVal * Time.deltaTime;
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rb = hit.collider.attachedRigidbody;
+        if (rb == null || rb.isKinematic) return;
+        rb.AddForceAtPosition(force * controller.velocity.normalized, hit.point);
+    }
+}
---
+++ Physics Based Game/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] myObjects;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int rand = Random.Range(0, myObjects.Length);
+        }
+    }
+}
